3785b9aedf762b427c25830e967743034a8d9a07
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-01-21 12:20:24 -0700

    adding javascript

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+$("#call").on("click", function(){
+  var title = $("#title").val();
+  var plot = $("#plot").val();
+  var responseType = $("#res").val();
 
+
+$.ajax({
+  url: 'https://www.omdbapi.com/?t='+title+'&y=&plot='+plot+'&r='+responseType,
+  method: "GET",
+  success: function(data) {
+    console.log(data);
+  }
+});
+
+});
